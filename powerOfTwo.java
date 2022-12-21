3b7c082fbeaff649af6296930ef26cab097992e0
Rishabhraj2754 <rishabhraj2754@gmail.com>
2022-12-21 17:52:34 +0530

    Adding: Asinment Day 5

---
+++ powerOfTwo.java
@@ -0,0 +1,14 @@
+public class powerOfTwo {
+    public static void main(String[] args) {
+        int result =1;
+        int base = 2;
+        for(int i=1; i<=31; i++) {
+            result = result * base;
+
+
+            System.out.println(result);
+        }
+
+
+    }
+}
